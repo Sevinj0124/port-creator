d2382db7f7210ffee09db0ee98a270721bb0c724
Sevinj0124 <Cavadova.24@icloud.com>
2024-02-02 21:23:19 +0400

    bootstrap ile portfolio creator

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+
+      breakpoints: {
+        320: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 50,
+        },
+      },
+  });
